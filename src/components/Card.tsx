6e9b5b049a7172108b70466ee4d24bb916e3298d
Rawad <rawadalideeb@gmail.com>
2026-09-25 02:33:41 +0400

    start

---
+++ src/components/Card.tsx
@@ -0,0 +1,17 @@
+import type { ReactNode } from "react"
+
+type CardProps = {
+    children?: ReactNode,
+    bg?: string
+}
+
+const Card = ({ children, bg = 'bg-gray-100' }: CardProps) => {
+    return (
+        <div className={` ${bg} p-6 rounded-lg shadow-md`}>
+            {children}
+        </div>
+    )
+}
+
+export default Card
+
